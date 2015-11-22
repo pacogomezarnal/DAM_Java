5fc23edfa6c267b80589ceec85743cc625da8a33
Paco Gomez <fjgomez@florida-uni.es>
2015-11-22 09:12:58 +0100

    Poniendo Orden

---
+++ PoniendoOrden/src/Principal.java
@@ -0,0 +1,12 @@
+import visual.*;
+
+public class Principal {
+	static Ventana1 v1;
+	static Ventana2 v2;
+	public static void main(String[] args) {
+		v1=new Ventana1();
+		v1.setVisible(true);
+	}
+	
+
+}
---
+++ PoniendoOrden/src/visual/Ventana1.java
@@ -0,0 +1,34 @@
+package visual;
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import logica.Jugador;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Ventana1 extends JFrame {
+
+	private JPanel contentPane;
+
+
+	/**
+	 * Create the frame.
+	 */
+	public Ventana1() {
+		setTitle("VENTANA 1");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		
+		Jugador j1=new Jugador();
+	}
+}
