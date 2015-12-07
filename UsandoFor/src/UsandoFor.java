181b8e3f0659f17fff15593d60afdfd18f2d1f1e
Paco Gomez <fjgomez@florida-uni.es>
2015-12-07 07:51:14 +0100

    Usando For

---
+++ UsandoFor/src/UsandoFor.java
@@ -0,0 +1,18 @@
+
+public class UsandoFor {
+
+	public static void main(String[] args) {
+		//Estructura de control for
+		//inciaalizacion;considicon;paso
+		for(int i=0;i<10;i++){//i<10?
+			System.out.println(i);
+		}//i++
+		for(int i=10;i>0;i=i-2){
+			int j=3;
+			System.out.println("variable i:"+i);
+			System.out.println("variable j:"+j);
+		}
+
+	}
+
+}
